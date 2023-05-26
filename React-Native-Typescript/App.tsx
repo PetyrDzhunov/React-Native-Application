5c7898045addb174f04693b47a84fbbf16cdb9f1
petardzhunov <petar.djunov@abv.bg>
2023-05-26 15:09:25 +0300

    Create good styles and work with state and text input

--- React-Native-Typescript/App.tsx
+++ React-Native-Typescript/App.tsx
@@ -1,13 +1,39 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const goalInputHandler = (enteredText: string) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add goal" />
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={addGoalHandler}
+        >
+          <Text style={styles.button}>Click me</Text>
+        </TouchableOpacity>
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -16,17 +42,35 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 8,
+  },
+  buttonContainer: {},
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+    paddingVertical: 13, // Add vertical padding
+    paddingHorizontal: 20, // Add horizontal padding
+    borderRadius: 3,
   },
 });
